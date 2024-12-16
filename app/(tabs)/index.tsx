a245c240ebf39b46222008e0e46760b150765a9f
Bryan P <bryanphilips@ymail.com>
2024-12-16 03:13:57 -0500

    Banner edits

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
-import { StyleSheet, TextInput } from "react-native";
+import { StyleSheet, TextInput, Pressable } from "react-native";
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
-import { MaterialCommunityIcons, Foundation } from "@expo/vector-icons";
+import {
+  MaterialCommunityIcons,
+  FontAwesome,
+  FontAwesome6,
+} from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { SearchBar } from "react-native-elements";
+import SearchBar from "@/components/SearchBar";
 
 import React from "react";
 import Colors from "@/constants/Colors";
@@ -13,7 +17,10 @@ import { overrideColorScheme as useColorScheme } from "@/components/overrideColo
 const colorScheme = useColorScheme() as "light" | "dark";
 
 export default function indexScreen() {
-  const [text, onChangeText] = React.useState("Useless Text");
+  const [text, onChangeText] = React.useState("");
+  const onPressFunction = () => {
+    onChangeText("");
+  };
   const alert = false;
   return (
     <>
@@ -35,44 +42,64 @@ export default function indexScreen() {
               color='white'
             />
           </View>
-          <View style={styles.header_search}>
-            <TextInput
-              style={styles.header_search}
-              onChangeText={onChangeText}
-              value={text}
-            />
+          <View style={{ ...styles.header_search_container, borderRadius: 50 }}>
+            <FontAwesome name='search' size={18} color='#2e2f5f' />
+            <View style={styles.search_field}>
+              <TextInput
+                onChangeText={onChangeText}
+                placeholder='Search'
+                value={text}
+                placeholderTextColor='#6d6d8f'
+                style={{ outline: "none" }}
+              />
+            </View>
+            <Pressable onPress={onPressFunction}>
+              <FontAwesome6 name='xmark-circle' size={18} color='#7b7c9a' />
+            </Pressable>
           </View>
         </View>
-        <View style={styles.body}>
-          <EditScreenInfo path='app/(tabs)/index.tsx' />
-        </View>
+      </View>
+      <View style={styles.body}>
+        <EditScreenInfo path='app/(tabs)/index.tsx' />
       </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  main: {
-    flexDirection: "column",
-    gap: 0,
+  main: {},
+  body: {
+    flex: 1,
+    justifyContent: "center",
   },
   container: {
     flexDirection: "column",
-    gap: 0,
-    height: "20%",
+    gap: 30,
     backgroundColor: Colors[colorScheme ?? "light"].bannerBackground,
+    paddingVertical: 25,
+    paddingHorizontal: 30,
   },
   header_icons: {
     backgroundColor: Colors[colorScheme ?? "light"].bannerBackground,
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
-    marginHorizontal: 30,
   },
-  header_search: {
-    marginHorizontal: "auto",
-    gap: 23,
+  header_search_container: {
+    backgroundColor: "#efeff7",
+    display: "flex",
+    flexDirection: "row",
+    gap: 10,
+    borderRadius: "20px",
     height: 30,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
   },
-  body: {},
+  search_field: {
+    flex: 1,
+    outline: "none",
+    backgroundColor: "#efeff7",
+    borderWidth: 0,
+  },
 });
---
+++ components/SearchBar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, View } from "@/components/Themed";
+import { StyleSheet } from "react-native";
+
+export default function SearchBar() {
+  return (
+    <>
+      <View style={styles.search}>
+        <Text>Hi</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  search: {},
+});
